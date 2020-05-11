645304afec3e09a7f02624722d32823eb6cae131
shubhamm07 <shubhammastkar07@gmail.com>
2020-05-11 16:43:39 +0530

    assignment 4 submission

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+
+var byeSpeaker = {
+	speakWord: "Good Bye",
+	speak: function(name){
+		console.log(byeSpeaker.speakWord + " " + name);
+	}
+};
+
---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,8 @@
+
+var helloSpeaker = {
+	speakWord: "hello",
+	speak: function(name){
+		console.log(helloSpeaker.speakWord + " " + name);
+	}
+
+};
